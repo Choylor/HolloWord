eb8018b921e2321433878795f95f0979ad392ee0
Choylor <45039339+Choylor@users.noreply.github.com>
2018-11-14 23:43:25 +0800

    增加测试方法

--- TestGit/src/com/sxt/git/MyGit.java
+++ TestGit/src/com/sxt/git/MyGit.java
@@ -4,8 +4,10 @@ package com.sxt.git;
 public class MyGit {
 	
 	public static void main(String[] args) {
-		System.out.println("世界你好，我来了");
+		System.out.println("脢脌陆莽脛茫潞脙拢卢脦脪脌麓脕脣");
 		System.out.println("hello,word");
 	}
-
+	public static sayHello(){
+	System.out.println("test,lihao");
+	}
 }
